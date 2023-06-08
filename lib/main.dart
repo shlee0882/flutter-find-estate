2864e273ac261da6b5985282ca86a4a2c884235f
이상현 <shlee0882@gmail.com>
2023-06-08 16:16:28 +0900

    특정 매물 확인

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,23 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_find_estate/provider/NotificationsProvider.dart';
 import 'package:flutter_find_estate/screen/NotificationChecklistScreen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.messageId}");
-  // Here, you can parse the message and show a notification or store it
+  print('A bg message just showed up :  ${message.messageId}');
 }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher'); // <- Default icon
+  final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(MyApp());
 }
 
@@ -26,6 +31,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Notification App',
         debugShowCheckedModeBanner: false,
+        routes: {
+          '/main': (context) => NotificationChecklistScreen(),
+        },
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/screen/NotificationChecklistScreen.dart
+++ lib/screen/NotificationChecklistScreen.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 import '../main.dart';
 import '../provider/NotificationsProvider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,12 +23,68 @@ class _NotificationChecklistScreenState
   bool _isSale = false;
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
+
   @override
   void initState() {
     super.initState();
+    _runWhileAppIsTerminated();
+    _createChannel();
+    _requestNotificationPermissions();
+
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid,);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+    
     _registerNotification();
     _setupMessaging();
     _fetchNotifications();
+  }
+
+  void _createChannel() async {
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      '370043057221', // id
+      'High Importance Notifications', // title
+      importance: Importance.max,
+    );
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              icon: '@mipmap/ic_launcher',
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  void _runWhileAppIsTerminated() async {
+    var details = await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+    if (details!.didNotificationLaunchApp) {
+      if (details.notificationResponse?.payload != null) {
+        Navigator.pushNamed(context,'/main');
+      }
+    }
+  }
+
+  void onDidReceiveNotificationResponse(NotificationResponse notificationResponse) async {
+    Navigator.pushNamed(context,'/main');
   }
 
   void _registerNotification() async {
@@ -62,22 +122,40 @@ class _NotificationChecklistScreenState
     );
   }
 
+  void _requestNotificationPermissions() async {
+    PermissionStatus permissionStatus = await Permission.notification.status;
+    if (!permissionStatus.isGranted) {
+      PermissionStatus permission = await Permission.notification.request();
+      if (permission.isGranted) {
+        // Do something with granted permission
+      } else {
+        // Show a dialog telling the user the permission was not granted.
+      }
+    }
+  }
+
+  // Call this method to update or create fcm token
+  void _updateOrCreateFcmTocken(String? fcmToken) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Check if device ID already exists
+    String? deviceId = prefs.getString('deviceId');
+    if (deviceId == null) {
+      // Generate new device ID if it does not exist and save it
+      var uuid = const Uuid();
+      deviceId = uuid.v1();
+      prefs.setString('deviceId', deviceId);
+    }
+    // Update or create fcm token
+    await FirebaseFirestore.instance.collection('devices').doc(deviceId).set({
+      'token': fcmToken,
+    });
+  }
+
   Future<void> _setupMessaging() async {
     await Firebase.initializeApp();
     var token = await FirebaseMessaging.instance.getToken();
     print("Firebase Messaging Token: $token");
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null) {
-        _showNotification(message);
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('Message clicked!');
-    });
+    _updateOrCreateFcmTocken(token);
   }
 
   Future<void> _fetchNotifications() async {
